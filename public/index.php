d8934748275f1189df14287b0545fbf7e9546a2e
ablo goita <ablo.goita2002@gmail.com>
2024-07-08 10:36:17 +0200

    Premier ajout

--- public/index.php
+++ public/index.php
@@ -1,7 +1,13 @@
 <?php
 
-require_once '../src/Entity/Test.php';
+// require_once '../src/Entity/Test.php';
+require_once '../src/Core/Autoloader.php';
 use App\Entity\Test;
+use App\Core\Autoloader;
+
+
+Autoloader::autoload();
+
 
 $test = new Test();
 echo $test->display();
